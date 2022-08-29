a97e4595541d622b6899bc19ef1408592ba9c088
Festus Ayeh-Datey <chrisaydat@gmail.com>
2022-08-29 12:30:24 +0000

    updates to codebase

--- main.py
+++ main.py
@@ -9,18 +9,31 @@ import time
 from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome(ChromeDriverManager(cache_valid_range=0).install())
-link = "https://appstoreconnect.apple.com/analytics/app/"
+link = "https://appstoreconnect.apple.com/analytics/app/d30/1608146534/overview?iaemeasure=totalDownloads"
 driver.get(link)
-def apple_login():
- driver.get("https://appstoreconnect.apple.com/analytics/app/")
-WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.ID,"aid-auth-widget-iFrame")))
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "account_name_text_field"))).send_keys("enter email")
-driver.find_element_by_id().click()
-WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.ID,"aid-auth-widget-iFrame")))
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "password_text_field"))).send_keys("enter password")
-#driver.find_element_by_id ("password_text_field").send_keys(""")
 
-driver.find_element_by_id ("sign_in_button").click()
+
+def apple_login():
+    driver.get(
+        "https://appstoreconnect.apple.com/analytics/app/d30/1608146534/overview?iaemeasure=totalDownloads")
+
+
+WebDriverWait(driver, 10).until(
+    EC.frame_to_be_available_and_switch_to_it((By.ID, "aid-auth-widget-iFrame")))
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+    (By.ID, "account_name_text_field"))).send_keys("chris@myladder.africa")
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+    (By.ID, "aid-auth-widget-iFrame"))).click()
+WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.ID, "sign-in"))).click()
+WebDriverWait(driver, 10).until(
+    EC.frame_to_be_available_and_switch_to_it((By.ID, "aid-auth-widget-iFrame")))
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+    (By.ID, "password_text_field"))).send_keys("sadat1SADAT@")
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+    (By.ID, "aid-auth-widget-iFrame"))).click()
+WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.ID, "sign-in"))).click()
 time.sleep(5)
 try:
     element_present = EC.presence_of_element_located((By.ID, 'container'))
@@ -28,27 +41,36 @@ try:
 
     content = driver.page_source
     soup = BeautifulSoup(content, 'html.parser')
-    Impressions =soup.find().text
-    Productpageviews = soup.find().text
-    Conversionrate = soup.find().text
-    Totaldownloads = soup.find().text
-    Sessionperactiveusers = soup.find().text
-    Crashes = soup.find().text
-    TotaldownloadsbyTerritory = soup.find().text
-    Totaldownloadsbysource = soup.find().text
-    Totaldownloadsbydevice = soup.find().text
-    Averageretention = soup.find().text
+    Impressions = soup.find('title', attrs={
+                            'class': 'Box-sc-18eybku-0 Text__BaseText-sc-4nqr4q-0 Text-sc-4nqr4q-1 dquphU'}).text
+    Productpageviews = soup.find('title', attrs={
+                                 'class': '_Box-sc-18eybku-0 Text__BaseText-sc-4nqr4q-0 Text-sc-4nqr4q-1 dquphU'}).text
+    Conversionrate = soup.find('title', attrs={
+                               'class': 'Box-sc-18eybku-0 Text__BaseText-sc-4nqr4q-0 Text-sc-4nqr4q-1 dquphU'}).text
+    Totaldownloads = soup.find('title', attrs={
+                               'class': 'Box-sc-18eybku-0 Text__BaseText-sc-4nqr4q-0 Text-sc-4nqr4q-1 dquphU'}).text
+    Sessionperactiveusers = soup.find('title', attrs={
+                                      'class': 'Box-sc-18eybku-0 Text__BaseText-sc-4nqr4q-0 Text-sc-4nqr4q-1 dquphU'}).text
+    Crashes = soup.find('title', attrs={
+                        'class': 'Box-sc-18eybku-0 Text__BaseText-sc-4nqr4q-0 Text-sc-4nqr4q-1 dquphU'}).text
+    TotaldownloadsbyTerritory = soup.find(
+        'title', attrs={'class': '_3qQ9m1'}).text
+    Totaldownloadsbysource = soup.find(
+        'title', attrs={'class': '_3qQ9m1'}).text
+    Totaldownloadsbydevice = soup.find(
+        'title', attrs={'class': '_3qQ9m1'}).text
+    Averageretention = soup.find('title', attrs={'class': '_3qQ9m1'}).text
     result = {
-        "Impressions" : Impressions,
-        "Product_page_views" : Productpageviews,
-        "conversion_rate" : Conversionrate,
-        "Totaldownloads" : Totaldownloads,
-        "Session_per_active_users" : Sessionperactiveusers,
-        "Crashes" : Crashes,
-        "Totaldownloads_by_Territory" : TotaldownloadsbyTerritory,
-        "Totaldownloads_by_source" : Totaldownloadsbysource,
-        "Totaldownloads_by_device" : Totaldownloadsbydevice,
-        "Average_retention" : Averageretention
+        "Impressions": Impressions,
+        "Product_page_views": Productpageviews,
+        "conversion_rate": Conversionrate,
+        "Totaldownloads": Totaldownloads,
+        "Session_per_active_users": Sessionperactiveusers,
+        "Crashes": Crashes,
+        "Totaldownloads_by_Territory": TotaldownloadsbyTerritory,
+        "Totaldownloads_by_source": Totaldownloadsbysource,
+        "Totaldownloads_by_device": Totaldownloadsbydevice,
+        "Average_retention": Averageretention
     }
     print(result)
 except TimeoutException:
